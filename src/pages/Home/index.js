8a39b58bc31bf36cde1f3b3e00ffd9cfe7b439e9
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2025-03-31 14:08:20 -0300

    Home Page, Details

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { Link } from 'react-router-dom';
+import './home.css'
+
 function Home() {
+  const [filmes, setFilmes] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/popular", {
+        params: {
+          api_key: process.env.REACT_APP_API_KEY,
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+      //console.log(response.data.results);
+      setFilmes(response.data.results.slice(0, 10));
+      setLoading(false);
+    }
+    loadFilmes();
+  }, []);
+
+  if(loading) {
+    return(
+      <div className="loading">
+        <h2>Carregando...</h2>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }

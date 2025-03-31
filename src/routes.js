8d04eb79301f22fb16839f6079dfff1ce28f22f9
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2025-03-31 14:54:16 -0300

    Favorites page

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
-import Header from "./components/Header";
+import Favoritos from "./pages/Favoritos";
 
 import Erro from "./pages/Erro";
 
@@ -12,6 +13,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
+        <Route path="/favoritos" element={<Favoritos />} />
         <Route path="*" element={<Erro />} />
       </Routes>
     </BrowserRouter>
